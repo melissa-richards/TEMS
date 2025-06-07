dacebcb11009a8b44ba4977e4e927d24e2c83747
Melissa  Richards <73000073+melissa-richards@users.noreply.github.com>
2025-06-07 15:18:16 -0400

    clean up code

--- script.js
+++ script.js
@@ -125,3 +125,14 @@ function setupRememberButton() {
 }
 setupRememberButton();
 
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        let displayMenu = false;
+        const menu = document.getElementById("socialHide");
+        const hamburgerMenu = document.getElementById("hamburger");
+        hamburgerMenu.addEventListener("click", function () {
+            displayMenu = !displayMenu;
+            menu.style.display = displayMenu ? 'block' : 'none';
+        });
+    }
+}
